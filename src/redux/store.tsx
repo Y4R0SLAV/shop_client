c21ad0192defd2cfe7e60967c8dc6aa9bddb3908
Y4ROSLAV <jeros1337@gmail.com>
2022-04-01 22:01:53 +0300

    add redux tools

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -2,6 +2,7 @@ import { applyMiddleware, combineReducers, createStore } from "redux"
 import appReducer from './reducers/appReducer'
 import thunkMiddleware from 'redux-thunk'
 import shopReducer from "./reducers/shopReducer"
+import {composeWithDevTools} from "redux-devtools-extension"
 
 let rootReducer = combineReducers({
   app: appReducer,
@@ -11,8 +12,6 @@ let rootReducer = combineReducers({
 type RootReducerType = typeof rootReducer
 export type AppStateType = ReturnType<RootReducerType>
 
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-// для редакс дев тулс с тупайскриптом не оч работает, но мне пока что не очень то и нужен
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunkMiddleware)))
 
 export default store
