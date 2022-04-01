ccbbd548c596de2900fa0df60ca2842c71edb14c
Y4ROSLAV <jeros1337@gmail.com>
2022-04-01 22:01:37 +0300

    now i get data from server here

--- src/pages/Shop/Shop.js
+++ src/pages/Shop/Shop.js
@@ -1,29 +1,34 @@
-import React, { useState } from "react"
+import React, { useEffect } from "react"
 import s from "./shop.module.css"
 import headerImg from "../../assets/shopPage/defaultShopHeader.png"
 import saleImg from "../../assets/shopPage/saleImg.png"
 import BottomNavigation from '../../components/BottomNavigation/BottomNavigation'
 import ShopItems from '../../components/ShopItems/ShopItems'
 
-import { useDispatch } from 'react-redux'
-import { requestProducts } from './../../redux/reducers/shopReducer'
+import { useDispatch, useSelector } from 'react-redux'
+import { selectProducts } from "../../redux/selectors/productSelector"
+
+import {
+  requestProducts, requestAccessories,
+  requestClothes, requestCollections
+} from './../../redux/reducers/shopReducer'
 
 const Shop = () => {
   // isSale буду брать из url (в роутс написано)
-  // shopItemsArray буду брать из стейта через хуки
+  const shopItemsArray = useSelector(selectProducts)
   const isSale = false
-  const shopItemsArray = []
 
   const dispatch = useDispatch()
 
+  useEffect(() => {
+    dispatch(requestAccessories())
+    dispatch(requestCollections())
+    dispatch(requestClothes())
+    dispatch(requestProducts())
+  }, []);
+
   return (
     <div className={s.wrapper}>
-
-      <button onClick={() => dispatch(
-        requestProducts(null, null, false)
-      )}>
-        ЧЕК ЧЕК ЧЕК
-      </button>
 
       {isSale
         ? <img className={s.headerImg} src={saleImg} alt="Sale" />
