2243f21ac6d9c0e6856f6375b051655503e992d3
Y4ROSLAV <jeros1337@gmail.com>
2022-04-03 22:59:55 +0300

    typescripted + added scrolling to top after "link to"

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -1,23 +1,23 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import routes from '../routes'
-import PageNotFound from './../pages/PageNotFound'
+import PageNotFound from '../pages/PageNotFound'
 import s from './appRouter.module.css'
+import ScrollToTop from './ScrollToTop'
+
 
 const AppRouter = () => {
   return (
     <div className={s.wrapper}>
+      
+      <ScrollToTop />
       <Routes>
         <Route path="*" element={<PageNotFound />} />
 
         {routes.map(({ path, Component }) => {
-          if (path === '/sales') {
-            return <Route key={path} path={path} element={< Component isSale={true} />} />
-          } else {
             return <Route key={path} path={path} element={< Component />} />
-          }
-        }
-        )}
+        })}
+
       </Routes>
     </div>
   )
