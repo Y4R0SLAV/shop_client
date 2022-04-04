f5d80e2754961b9067de03278468ab154f98e29e
Y4ROSLAV <jeros1337@gmail.com>
2022-04-04 16:54:26 +0300

    rename

--- src/assets/links.tsx
+++
@@ -1,2 +0,0 @@
-export const vkURL = "https://vk.com/ykorablev"
-export const instagamURL = "https://instagram.com/y4r0cl4v"
---
+++ src/assets/linksAndConstants.tsx
@@ -0,0 +1,5 @@
+export const vkURL = "https://vk.com/ykorablev"
+export const instagamURL = "https://instagram.com/y4r0cl4v"
+
+export const clothesId = 4;
+export const accessoriesid = 5;
