bd74265f11a5363387a46e09fc63d5759a608161
Y4ROSLAV <jeros1337@gmail.com>
2022-04-14 19:49:35 +0300

    added some logic

--- src/pages/Shop/Shop.tsx
+++ src/pages/Shop/Shop.tsx
@@ -10,25 +10,42 @@ import { selectProducts } from "../../redux/selectors/productSelector"
 
 import { requestProducts } from '../../redux/reducers/shopReducer'
 import { SALES_ROUTE } from "../../routes"
+import { selectCollections } from "../../redux/selectors/collectionSelector"
 
 const Shop = () => {
-  let location = useLocation()
-  const shopItemsArray = useSelector(selectProducts)
-  const isSale = location.pathname === SALES_ROUTE
-
+  const location = useLocation()
   const dispatch = useDispatch()
 
+  const isSale = location.pathname === SALES_ROUTE
+  const typeOfPage = location.pathname.split('/')[1] || undefined
+  const id = +location.pathname.split('/')[2] || undefined
+
+  const curPageIsCollections = typeOfPage === 'collections'
+  const collections = useSelector(selectCollections)
+  let headerUrl = headerImg
+
+  if (curPageIsCollections && collections.length > 0) { 
+    headerUrl = collections.filter(item => item.id === id)[0].header 
+  } else if (isSale) { 
+    headerUrl = saleImg 
+  } else if (typeOfPage !== undefined) {
+    // я либо в одеждах либо в аксессуарах
+    headerUrl = ''
+  }
+
+  const shopItemsArray = useSelector(selectProducts)
+
   useEffect(() => {
-    dispatch(requestProducts(undefined, undefined, isSale))
+    if (curPageIsCollections) {
+      dispatch(requestProducts(undefined, id, isSale))
+    } else {
+      dispatch(requestProducts(id, undefined,isSale))
+    }
   }, [location.pathname]);
 
   return (
     <div className={s.wrapper}>
-
-      {isSale
-        ? <img className={s.headerImg} src={saleImg} alt="Sale" />
-        : <img className={s.headerImg} src={headerImg} alt="Ovsyankin Boutique" />
-      }
+      {headerUrl !== '' && <img className={s.headerImg} src={headerUrl} alt="" />}
 
       <ShopItems shopItemsArray={shopItemsArray}/>
     </div>
